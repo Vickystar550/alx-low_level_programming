656f77114feb090c606364e5d1752637f8618751
Victor Nice <victornice550@gmail.com>
2022-07-03 07:32:11 -0700

    Check 4-puts.c script with betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) /* This function print to stdout using puts() */
+/**
+ * main - Entry point
+ *
+ * Description: this program print to the stdout using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char myString[] = "Programming is like building a multilingual puzzle";
+	char myString[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(myString);
 
 	return (0);
